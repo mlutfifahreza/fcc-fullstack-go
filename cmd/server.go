97a32c5b889239f3deb5bfe6005957ee3962d4a8
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2024-11-02 23:02:20 +0700

    fix: dot env (#3)

--- cmd/server.go
+++ cmd/server.go
@@ -2,9 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
+
+	"github.com/joho/godotenv"
 
 	"github.com/mlutfifahreza/fcc-fullstack-go/api"
 	"github.com/mlutfifahreza/fcc-fullstack-go/internal/product_db"
@@ -14,12 +18,33 @@ type AppDependencies struct {
 	productDB *product_db.Database
 }
 
+type EnvConfig struct {
+	// App
+	ENV  string
+	PORT int
+
+	// Database
+	PRODUCT_DB_HOST     string
+	PRODUCT_DB_PORT     int
+	PRODUCT_DB_USER     string
+	PRODUCT_DB_PASSWORD string
+	PRODUCT_DB_NAME     string
+}
+
 func RunServer() {
 	// Initialize Fiber app
 	app := fiber.New()
 
 	// Initialize Dependencies
-	dsn := "postgres://username:password@localhost:5432/product_db"
+	envConfig := loadEnv()
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%d/%s",
+		envConfig.PRODUCT_DB_USER,
+		envConfig.PRODUCT_DB_PASSWORD,
+		envConfig.PRODUCT_DB_HOST,
+		envConfig.PRODUCT_DB_PORT,
+		envConfig.PRODUCT_DB_NAME,
+	)
 	productDB, err := product_db.NewDatabase(dsn)
 	if err != nil {
 		log.Fatalf("Failed to initialize database: %v", err)
@@ -32,7 +57,34 @@ func RunServer() {
 	setupRoute(app, dep)
 
 	// Run
-	log.Fatal(app.Listen(fmt.Sprintf(":%d", 8000)))
+	log.Fatal(app.Listen(fmt.Sprintf(":%d", envConfig.PORT)))
+}
+
+func loadEnv() *EnvConfig {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		log.Fatalf("Error loading .env PORT")
+	}
+
+	dbPort, err := strconv.Atoi(os.Getenv("PRODUCT_DB_PORT"))
+	if err != nil {
+		log.Fatalf("Error loading .env PRODUCT_DB_PORT")
+	}
+
+	return &EnvConfig{
+		ENV:                 os.Getenv("ENV"),
+		PORT:                port,
+		PRODUCT_DB_HOST:     os.Getenv("PRODUCT_DB_HOST"),
+		PRODUCT_DB_PORT:     dbPort,
+		PRODUCT_DB_USER:     os.Getenv("PRODUCT_DB_USER"),
+		PRODUCT_DB_PASSWORD: os.Getenv("PRODUCT_DB_PASSWORD"),
+		PRODUCT_DB_NAME:     os.Getenv("PRODUCT_DB_NAME"),
+	}
 }
 
 func setupRoute(app *fiber.App, dep *AppDependencies) {
@@ -43,6 +95,4 @@ func setupRoute(app *fiber.App, dep *AppDependencies) {
 	app.Patch("/products", api.HandleUpdateProduct(dep.productDB))
 	app.Get("/products/:id", api.HandleGetProduct(dep.productDB))
 	app.Delete("/products/:id", api.HandleDeleteProduct(dep.productDB))
-
-	log.Info("Routes are setup")
 }
